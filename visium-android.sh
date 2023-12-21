2bda1de249791d10acc147120c8a86b54d59e3ba
Brian Rakhmat Aji <brianetlab@gmail.com>
2023-12-21 14:29:46 +0700

    Update visium-android.sh

--- visium-android.sh
+++ visium-android.sh
@@ -25,7 +25,7 @@ fi
 # Make the GET request and capture the JSON response
 response=$(curl -s -X GET https://farmdemo.visiumlabs.com/api/devices?os=Android \
 -H "Content-Type: application/json" \
--H "X-VisiumFarm-Api-Key: xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE")
+-H "X-VisiumFarm-Api-Key: Fqxr7pj3Pt.fcHvJMeCzbdTxK5Mu8haeJ2CL0iDASQddsRD0WsU")
 
 # Extract the 'id' attributes from the JSON response and format output with double quotes
 ids=$(echo "$response" | jq -r '[.[] | .deviceId] | map("\"" + . + "\"") | join(",")')
@@ -39,7 +39,7 @@ export ids=$ids
 
 # step 3: upload APK file and get the id of APK file
 # Set the necessary variables
-API_KEY="xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE"
+API_KEY="Fqxr7pj3Pt.fcHvJMeCzbdTxK5Mu8haeJ2CL0iDASQddsRD0WsU"
 FILE_PATH="Simple_Notepad_2.0.1_Apkpure.apk"
 API_URL="https://farmdemo.visiumlabs.com/api/v1/apps"
 
@@ -61,7 +61,7 @@ export appId=$appId
 # step 4: Install the APK in all Android devices
 curl -X POST https://farmdemo.visiumlabs.com/api/apk/install \
 -H "Content-Type: application/json" \
--H "X-VisiumFarm-Api-Key: xmOo8Z5tp6.d9DQNq6nhbC9vxy2tDr32tfVBxUl6wKhpxSAiHtE" \
+-H "X-VisiumFarm-Api-Key: Fqxr7pj3Pt.fcHvJMeCzbdTxK5Mu8haeJ2CL0iDASQddsRD0WsU" \
 -d '{
   "appList": [
     {
